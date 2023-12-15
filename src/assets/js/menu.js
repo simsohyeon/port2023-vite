d7e92ee036b1d4b5e2b222baede3c835fb717bab
simsohyeon <80887004+simsohyeon@users.noreply.github.com>
2023-12-15 14:04:56 +0900

    😜 헤더 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,13 @@
 export function menu() { //export라고 쓰면 내보낼 수 있음
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
+
+    if (headerToggle) {
+        headerToggle.addEventListener("click", () => { 
+        headerNav.classList.toggle("show"); // headerNav를 클릭하면, show를 보여준다.
+        headerToggle.getAttribute("aria-expanded") === "true" // headerToggle의 속성값 aria-expanded 값이 true이면,
+        ? headerToggle.setAttribute("aria-expanded", "false") // true인 경우에는 aria-expanded의 값을 false로 
+        : headerToggle.setAttribute("aria-expanded", "true"); // false인 경우에는 aria-expanded의 값을 true로 변경해준다.  
+        });
+    }
 }
